beda09331edb989a60c68471554a599e518a92a0
Luke <novelview9@example.com>
2016-12-26 17:17:35 +0900

    add debug and developement tools

--- tang/tang/settings/development.py
+++ tang/tang/settings/development.py
@@ -2,6 +2,7 @@ from .partials import *
 
 INSTALLED_APPS += [
         'debug_toolbar',
+        'django_extensions',        
 ]
 
 MIDDLEWARE += [
--- tang/tang/settings/partials/databases.py
+++ tang/tang/settings/partials/databases.py
@@ -1,15 +1,18 @@
 import os
 
-from .base import BASE_DIR
+import os
 
+import dj_database_url
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(default=os.environ.get('DATABASE_URL'), )
